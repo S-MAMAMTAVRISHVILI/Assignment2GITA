d92032cd180f3a73d6d0f54187ebf8741d79253f
S-MAMAMTAVRISHVILI <147278016+S-MAMAMTAVRISHVILI@users.noreply.github.com>
2025-09-13 23:20:33 +0400

    Update GetDigitsSum.js for negative numbers

--- GetDigitsSum.js
+++ GetDigitsSum.js
@@ -1,4 +1,5 @@
 function getDigitsSum(num) {
+    num = Math.abs(num)
     if(num / 10 < 1) {
         return num
     }
@@ -7,7 +8,9 @@ function getDigitsSum(num) {
 
 //few checks
 console.log(getDigitsSum(123))
+console.log(getDigitsSum(-123))
 console.log(getDigitsSum(7))
+console.log(getDigitsSum(-7))
 console.log(getDigitsSum(0))
 console.log(getDigitsSum(19))
 console.log(getDigitsSum(123456789))
